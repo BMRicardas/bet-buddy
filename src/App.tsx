eb6bf4f77a19db22df11f352d3aa39008dd77ba7
Ričardas Brazdžius <ricardas.brazdzius@gmail.com>
2025-04-06 20:48:14 +0300

    add BalanceProvider and context for managing user balance

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,29 @@ import { AuthProvider } from "./contexts/auth.context";
 import { ProtectedRoute } from "./routes/protected.route";
 import { HomePage } from "./pages/home.page";
 import { PublicRoute } from "./routes/public.route";
+import { BalanceProvider } from "./contexts/balance.context";
 
 export function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Public Routes */}
-          <Route Component={PublicRoute}>
-            <Route path="/login" Component={LoginPage} />
-            <Route path="/register" Component={RegisterPage} />
-          </Route>
-          {/* Protected Routes */}
-          <Route Component={ProtectedRoute}>
-            <Route path="/" Component={HomePage} />
-            <Route path="/dashboard" Component={() => <h1>Dashboard</h1>} />
-          </Route>
-          {/* Not Found */}
-          <Route path="*" Component={() => <h1>404 Not Found</h1>} />
-        </Routes>
-      </BrowserRouter>
+      <BalanceProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* Public Routes */}
+            <Route Component={PublicRoute}>
+              <Route path="/login" Component={LoginPage} />
+              <Route path="/register" Component={RegisterPage} />
+            </Route>
+            {/* Protected Routes */}
+            <Route Component={ProtectedRoute}>
+              <Route path="/" Component={HomePage} />
+              <Route path="/dashboard" Component={() => <h1>Dashboard</h1>} />
+            </Route>
+            {/* Not Found */}
+            <Route path="*" Component={() => <h1>404 Not Found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </BalanceProvider>
     </AuthProvider>
   );
 }
---
+++ src/contexts/balance.context.tsx
@@ -0,0 +1,41 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useAuth } from "./auth.context";
+
+interface BalanceContextType {
+  balance: number;
+  updateBalance: (newBalance: number) => void;
+}
+
+const BalanceContext = createContext<BalanceContextType | undefined>(undefined);
+
+export function BalanceProvider({ children }: { children: ReactNode }) {
+  const { user } = useAuth();
+  const [balance, setBalance] = useState(user?.balance || 0);
+
+  const updateBalance = (newBalance: number) => {
+    setBalance(() => newBalance);
+  };
+
+  const value = {
+    balance,
+    updateBalance,
+  };
+
+  return (
+    <BalanceContext.Provider value={value}>{children}</BalanceContext.Provider>
+  );
+}
+
+export function useBalance() {
+  const context = useContext(BalanceContext);
+  if (!context) {
+    throw new Error("useBalance must be used within a BalanceProvider");
+  }
+  return context;
+}
